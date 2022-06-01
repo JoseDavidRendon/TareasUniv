b4ccd6a40b8bb42403b6553190a47aa0cb4616b7
David <exito387@gmail.com>
2022-06-01 16:59:43 -0500

    login y register completos

--- TareasUnivProyecto/index/views.py
+++ TareasUnivProyecto/index/views.py
@@ -2,9 +2,9 @@ from pyexpat.errors import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators  import login_required
-from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 # Create your views here.
@@ -14,18 +14,21 @@ def inicio(request):
     return render(request, 'index/index.html')
 
 def registro(request):
-    data={
-        'form':CustomUserCreationForm()
-        }
+    error= False
     if request.method =='POST':
-        formulario = CustomUserCreationForm(data=request.POST)
+        formulario = UserRegisterForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
             user=authenticate(username=formulario.cleaned_data['username'], password=formulario.cleaned_data['password1'])
             login(request, user)
             messages.success(request, 'Profile details updated.')
             return redirect(to="/")
-            
+        else:
+            error=True
+    data={
+        'form':UserRegisterForm(),
+        'error':error
+        }       
     return render(request, 'registration/registro.html', data)
 
 def salir(request):
