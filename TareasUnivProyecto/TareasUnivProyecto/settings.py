c76ac56a1a588cb9312b400e4be4efef4cf573d3
David <exito387@gmail.com>
2022-06-24 07:16:13 -0500

    Update settings.py

--- TareasUnivProyecto/TareasUnivProyecto/settings.py
+++ TareasUnivProyecto/TareasUnivProyecto/settings.py
@@ -135,13 +135,13 @@ LOGOUT_REDIRECT_URL = "/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP  = os.path.join(BASE_DIR, 'static') 
-STATIC_URL  = '/static/'
+#STATIC_URL  = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
