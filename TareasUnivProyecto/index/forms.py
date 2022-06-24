1a97fc9b6a200baeb152000c68ef49c4319134e9
David <exito387@gmail.com>
2022-06-24 06:51:24 -0500

    Update forms.py

--- TareasUnivProyecto/index/forms.py
+++ TareasUnivProyecto/index/forms.py
@@ -1,6 +1,5 @@
 from dataclasses import fields
 import datetime
-from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
