ce6c10fe2d9293d66a57500c0aa703d8d7634ec6
David <exito387@gmail.com>
2022-06-27 22:16:38 -0500

    last

--- TareasUnivProyecto/TareasUnivProyecto/urls.py
+++ TareasUnivProyecto/TareasUnivProyecto/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns += [
+    path(settings.STATIC_URL[1:], serve, {'document_root': settings.STATIC_ROOT })
+]
